4ee2b44e569dad2c2d7c6dc0f64300c3089b1445
h-kanta <uta_082812@icloud.com>
2024-02-18 10:29:44 +0900

    Initial Commit

---
+++ SwiftExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftExample
+//
+//  Created by 堀川貫太 on 2024/02/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftExample/SwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExampleApp.swift
+//  SwiftExample
+//
+//  Created by 堀川貫太 on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
